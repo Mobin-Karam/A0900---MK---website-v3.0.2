8c1500a9f5bbba4ae44949c067905215c80b41b6
Mobin Karam <polyt.mobin.karam@gmail.com>
2023-06-05 18:22:52 +0330

    fixed my error and make spacing extend in tailwind css config.js

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,50 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+  BrowserRouter,
+  Routes,
+  Outlet,
+} from "react-router-dom";
+// it should use React Router Dom
+import "./dist/main.css";
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
+
+const App = () => {
+  return (
+    <div className="app">
+      <div className="container">
+        <RouterProvider router={router} />
+      </div>
+    </div>
+  );
+};
+
+export default App;
