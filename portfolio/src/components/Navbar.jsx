388dccece50fefe42a7cd29a22602b0dfad71c5f
Mobin Karam <polyt.mobin.karam@gmail.com>
2023-06-06 00:27:53 +0330

    start styling my website portfolio

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -1,9 +1,46 @@
 import React from "react";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+  BrowserRouter,
+  Routes,
+  Outlet,
+} from "react-router-dom";
 
 const Navbar = () => {
   return (
     <>
-      <div className="w-full h-10 bg-slate-500"></div>
+      <nav className="Nav">
+        <Link className="Link Logo" to={"/"}>
+          <h1 className="LogoText">Mobin.dev</h1>
+        </Link>
+        <div className="Navbar"> 
+          <Link className="Link" to={"/"}>
+            <span className="item">Home</span>
+          </Link>
+          <select className="DropDown">
+            <option value="">Work</option>
+            <option value="work-2">Work-2</option>
+            <option value="work-3">Work-3</option>
+            <option value="work-4">Work-4</option>
+            <option value="work-5">Work-5</option>
+          </select>
+          <Link className="Link" to={"/about"}>
+            <span className="item">About</span>
+          </Link>
+          <Link className="Link" to={"/projects"}>
+            <span className="item">Projects</span>
+          </Link>
+          <Link className="Link" to={"/services"}>
+            <span className="item">Services</span>
+          </Link>
+        </div>
+        <Link className="Link">
+          <button className="Btn">Let's chat</button>
+        </Link>
+      </nav>
     </>
   );
 };
--- portfolio/src/pages/Home.jsx
+++ portfolio/src/pages/Home.jsx
@@ -5,10 +5,63 @@ const Home = () => {
   return (
     <>
       <Helmet>
-        <title>Home</title>
-        <meta name="description" content="Frontend develoepr | Backend developer | Full Stack Developer" />
+        <title>Home | Mobin Karam</title>
+        <meta
+          name="description"
+          content="Frontend develoepr | Backend developer | Full Stack Developer"
+        />
       </Helmet>
-      <div className="">Home</div>
+      <section className="Home">
+        <section className="Center">
+          <div className="Left">
+            <div className="LeftTop">
+              <h1 className="TitleLeftTop"></h1>
+              <p className="PLeftTop"></p>
+              <div className="BtnLeftTop">
+                <button className="Btn"></button>
+                <button className="Btn"></button>
+              </div>
+              <div className="NumLeftTop">
+                <div className="NumberBox">
+                  <div className="Number"></div>
+                  <div className="NumberText"></div>
+                </div>
+                <div className="NumberBox">
+                  <div className="Number"></div>
+                  <div className="NumberText"></div>
+                </div>
+                <div className="NumberBox">
+                  <div className="number"></div>
+                  <div className="numberText"></div>
+                </div>
+              </div>
+            </div>
+            <div className="LeftBottom">
+              <div className="BoxOne">
+                <h2 className="TitleLeftBottom">Project</h2>
+                <button className="Btn">Know More</button>
+              </div>
+              <div className="BoxTwo">
+                <h2 className="TextLeftBottom">
+                  <p className="Txt"></p>
+                  <p className="Num"></p>
+                </h2>
+                <h2 className="TextLeftBottom">
+                  <p className="Txt"></p>
+                  <p className="Num"></p>
+                </h2>
+                <h2 className="TextLeftBottom">
+                  <p className="Txt"></p>
+                  <p className="Num"></p>
+                </h2>
+              </div>
+            </div>
+          </div>
+          <div className="Right">
+            <p className="TitlePhoto">MyPersonalPhoto</p>
+          </div>
+        </section>
+      </section>
     </>
   );
 };
